d65f541ba954a1be14f9c50edf8e50d99a38c49d
limchaweisp2 <limchaweisp2@gmail.com>
2026-01-27 14:32:31 +0800

    Delete cicd_learning_course/set4 directory

--- cicd_learning_course/set4/preprocess.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-# Simulating the preprocessing stage
-df = pd.read_csv("weather.csv")
-df.to_csv("processed_data.csv", index=False)
-print("Preprocessing complete.")
--- cicd_learning_course/set4/train.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-# Simulating training stage
-df = pd.read_csv("processed_data.csv")
-print("Training complete. Saving plots dummy.")
-with open("plots.png", "w") as f:
-    f.write("dummy plot")
